9923b54c610d0283323c48a9d877b741529e4632
Gabriel Sperrer <gabriel@drimble.app>
2023-02-07 13:55:41 +0100

    feat: use transactions for database changes (#16)
    To make sure the database stays consistent, changs to drinks that
    require changes to diary entries should happen within a transaction (and
    vice versa).

--- lib/data/drinks_repository.dart
+++ lib/data/drinks_repository.dart
@@ -6,39 +6,42 @@ import 'daos/diary_dao.dart';
 import 'daos/drinks_dao.dart';
 
 class DrinksRepository {
-  final DrinksDAO _consumedDrinksDao;
+  final DrinksDAO _drinksDao;
   final DiaryDAO _diaryDao;
 
-  DrinksRepository(this._consumedDrinksDao, this._diaryDao);
+  DrinksRepository(this._drinksDao, this._diaryDao);
 
   Stream<List<Drink>> observeDrinksBetween(DateTime startDate, DateTime endDate) =>
-      _consumedDrinksDao.observeBetweenDates(startDate, endDate);
+      _drinksDao.observeBetweenDates(startDate, endDate);
 
-  Stream<List<Drink>> observeDrinksOnDate(DateTime date) => _consumedDrinksDao.observeOnDate(date);
+  Stream<List<Drink>> observeDrinksOnDate(DateTime date) => _drinksDao.observeOnDate(date);
 
-  Future<List<Drink>> getDrinksOnDate(DateTime date) async => _consumedDrinksDao.findOnDate(date);
+  Stream<List<Drink>> observeLatestDrinks() => _drinksDao.observeLatest();
 
-  Stream<List<Drink>> observeLatestDrinks() => _consumedDrinksDao.observeLatest();
+  Future<List<Drink>> findDrinksOnDate(DateTime date) => _drinksDao.findOnDate(date);
 
   void save(Drink drink) async {
-    await _consumedDrinksDao.save(drink);
-    await _markAsNonDrinkFree(drink.date);
-  }
-
-  void removeDrink(Drink drink) async {
-    await _consumedDrinksDao.delete(drink);
-
-    final remainingDrinksOnThisDay = await getDrinksOnDate(drink.date);
-    if (remainingDrinksOnThisDay.isEmpty) {
-      await _markAsUntracked(drink.date);
-    }
+    await _drinksDao.transaction(() async {
+      await _drinksDao.save(drink);
+      await _markAsNonDrinkFree(drink.date);
+    });
   }
 
   Future<void> _markAsNonDrinkFree(DateTime date) async {
     final existingEntry = await _diaryDao.findOnDate(date);
     final diaryEntry = existingEntry?.copyWith(isDrinkFreeDay: false) ?? DiaryEntry(date: date, isDrinkFreeDay: false);
+    await _diaryDao.save(diaryEntry);
+  }
 
-    _diaryDao.save(diaryEntry);
+  void removeDrink(Drink drink) async {
+    await _drinksDao.transaction(() async {
+      await _drinksDao.delete(drink);
+
+      final remainingDrinksOnThisDay = await _drinksDao.findOnDate(drink.date);
+      if (remainingDrinksOnThisDay.isEmpty) {
+        await _markAsUntracked(drink.date);
+      }
+    });
   }
 
   Future<void> _markAsUntracked(DateTime date) async {
