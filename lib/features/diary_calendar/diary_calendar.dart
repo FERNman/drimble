09e856638418af55a148640d93448ea5bebdecaa
Jakob Staudinger <jakobstaudi@gmail.com>
2024-02-25 17:31:58 +0100

    update diary calendar based on visible days

--- lib/features/diary_calendar/diary_calendar.dart
+++ lib/features/diary_calendar/diary_calendar.dart
@@ -25,33 +25,54 @@ class DiaryCalendar extends StatelessWidget {
         builder: (context, state) => SizedBox(
           height: 100,
           child: LayoutBuilder(
-            builder: (context, constraints) => ListView.builder(
-              shrinkWrap: true,
-              controller: _scrollController,
-              scrollDirection: Axis.horizontal,
-              reverse: true,
-              padding: const EdgeInsets.symmetric(horizontal: padding),
-              itemBuilder: (context, dayIndex) {
-                final adjustedDayIndex = dayIndex - futureDayCount;
-                final date = today.subtract(days: adjustedDayIndex);
+            builder: (context, constraints) => NotificationListener(
+              child: ListView.builder(
+                shrinkWrap: true,
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                padding: const EdgeInsets.symmetric(horizontal: padding),
+                itemBuilder: (context, dayIndex) {
+                  final adjustedDayIndex = dayIndex - futureDayCount;
+                  final date = today.subtract(days: adjustedDayIndex);
 
-                return DiaryCalendarDay(
-                  date: date,
-                  isSelected: selectedDay == date,
-                  isDrinkFreeDay: state.isDrinkFreeDay(date),
-                  onTap: date.isAfter(today)
-                      ? null
-                      : () {
-                          _scrollTo(adjustedDayIndex, constraints);
-                          onSelectedDayChanged(date);
-                        },
-                );
+                  return DiaryCalendarDay(
+                    date: date,
+                    isSelected: selectedDay == date,
+                    isDrinkFreeDay: state.isDrinkFreeDay(date),
+                    onTap: date.isAfter(today)
+                        ? null
+                        : () {
+                            _scrollTo(adjustedDayIndex, constraints);
+                            onSelectedDayChanged(date);
+                          },
+                  );
+                },
+              ),
+              onNotification: (notification) {
+                if (notification is ScrollNotification) {
+                  _updateVisibleRange(context, constraints, notification);
+                  return true;
+                }
+                return false;
               },
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _updateVisibleRange(BuildContext context, BoxConstraints constraints, ScrollNotification notification) {
+    final endPixels = notification.metrics.pixels;
+    final startPixels = notification.metrics.pixels + constraints.maxWidth;
+
+    final endIndex = ((endPixels - padding) / DiaryCalendarDay.width).floor() - futureDayCount;
+    final startIndex = ((startPixels - padding) / DiaryCalendarDay.width).ceil() - futureDayCount;
+
+    context
+        .read<DiaryCalendarCubit>()
+        .updateRange(Date.today().subtract(days: startIndex), Date.today().subtract(days: endIndex));
   }
 
   void _scrollTo(int index, BoxConstraints constraints) {
