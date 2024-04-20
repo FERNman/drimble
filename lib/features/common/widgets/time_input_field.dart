05485a84f3a731465d81424ce11e7fccf32198af
Jakob Staudinger <jakobstaudi@gmail.com>
2024-04-20 15:33:49 +0200

    improve regex

--- lib/features/common/widgets/time_input_field.dart
+++ lib/features/common/widgets/time_input_field.dart
@@ -6,7 +6,7 @@ import '../build_context_extensions.dart';
 class TimeInputField extends StatelessWidget {
   static final _inputRegex = RegExp(r'^[0-9:\sAPM]*$', caseSensitive: false);
   static final _timespanRegex = RegExp(
-      r'^\s*(?<hour>(?:[01]?\d)|(?:2[0-3])):(?<minute>[0-5]\d)(?:\s?(?<modifier>[AP]M))?\s*$',
+      r'^\s*((?<hour12>(0?[1-9])|(1[0-2])):(?<minute12>([0-5])\d)\s*(?<modifier>[AP]M))|((?<hour24>((0?|1)\d)|(2[0-3])):(?<minute24>([0-5])\d))\s*$',
       caseSensitive: false);
 
   final TimeOfDay initialValue;
@@ -37,20 +37,41 @@ class TimeInputField extends StatelessWidget {
   void _onChanged(String rawValue) {
     final match = _timespanRegex.firstMatch(rawValue);
     if (match != null) {
-      final hour = match.namedGroup('hour');
-      final minute = match.namedGroup('minute');
-      final modifier = match.namedGroup('modifier');
+      final time = getTime(match);
 
-      if (hour != null && minute != null) {
-        var hours = int.parse(hour);
-        final minutes = int.parse(minute);
-
-        if (modifier?.toLowerCase() == 'pm') {
-          hours += 12;
-        }
-
+      if (time != null) {
+        final (hours, minutes) = time;
         onChanged(TimeOfDay(hour: hours, minute: minutes));
       }
     }
   }
+}
+
+(int, int)? getTime(RegExpMatch match) {
+  final modifier = match.namedGroup('modifier');
+
+  if (modifier != null) {
+    final hour = match.namedGroup('hour12');
+    final minute = match.namedGroup('minute12');
+
+    if (hour != null && minute != null) {
+      final hourModification = modifier.toLowerCase() == 'pm' ? 12 : 0;
+      final hours = int.parse(hour) + hourModification;
+      final minutes = int.parse(minute);
+
+      return (hours, minutes);
+    }
+  } else {
+    final hour = match.namedGroup('hour24');
+    final minute = match.namedGroup('minute24');
+
+    if (hour != null && minute != null) {
+      final hours = int.parse(hour);
+      final minutes = int.parse(minute);
+
+      return (hours, minutes);
+    }
+  }
+
+  return null;
 }
