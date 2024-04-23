c39944df4032d83e413ba21cdef5d1ca8dc4cd62
Jakob Staudinger <jakobstaudi@gmail.com>
2024-04-23 10:46:31 +0200

    Improve time input (#69)

--- lib/features/common/widgets/time_input_field.dart
+++ lib/features/common/widgets/time_input_field.dart
@@ -4,8 +4,10 @@ import 'package:flutter/services.dart';
 import '../build_context_extensions.dart';
 
 class TimeInputField extends StatelessWidget {
-  static final _digitsAndColonRegex = RegExp(r'^[0-9:]*$');
-  static final _timespanRegex = RegExp(r'^(([01]\d)|(2[0-3])):[0-5]\d$');
+  static final _inputRegex = RegExp(r'^[0-9:\sAPM]*$', caseSensitive: false);
+  static final _timespanRegex = RegExp(
+      r'^\s*((?<hour12>(0?[1-9])|(1[0-2])):(?<minute12>([0-5])\d)\s*(?<modifier>[AP]M))|((?<hour24>((0?|1)\d)|(2[0-3])):(?<minute24>([0-5])\d))\s*$',
+      caseSensitive: false);
 
   final TimeOfDay initialValue;
   final ValueChanged<TimeOfDay> onChanged;
@@ -22,9 +24,9 @@ class TimeInputField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       initialValue: initialValue.format(context),
-      keyboardType: const TextInputType.numberWithOptions(decimal: false),
+      keyboardType: TextInputType.datetime,
       inputFormatters: [
-        FilteringTextInputFormatter.allow(_digitsAndColonRegex),
+        FilteringTextInputFormatter.allow(_inputRegex),
       ],
       validator: (value) => _timespanRegex.hasMatch(value!) ? null : context.l18n.common_invalidTimeFormat,
       decoration: decoration,
@@ -33,12 +35,43 @@ class TimeInputField extends StatelessWidget {
   }
 
   void _onChanged(String rawValue) {
-    if (_timespanRegex.hasMatch(rawValue)) {
-      final timeParts = rawValue.split(':');
-      final hours = int.parse(timeParts[0]);
-      final minutes = int.parse(timeParts[1]);
+    final match = _timespanRegex.firstMatch(rawValue);
+    if (match != null) {
+      final time = getTime(match);
 
-      onChanged(TimeOfDay(hour: hours, minute: minutes));
+      if (time != null) {
+        final (hours, minutes) = time;
+        onChanged(TimeOfDay(hour: hours, minute: minutes));
+      }
     }
   }
+}
+
+(int, int)? getTime(RegExpMatch match) {
+  final modifier = match.namedGroup('modifier');
+
+  if (modifier != null) {
+    final hour = match.namedGroup('hour12');
+    final minute = match.namedGroup('minute12');
+
+    if (hour != null && minute != null) {
+      final hourModification = modifier.toLowerCase() == 'pm' ? 12 : 0;
+      final hours = int.parse(hour) + hourModification;
+      final minutes = int.parse(minute);
+
+      return (hours, minutes);
+    }
+  } else {
+    final hour = match.namedGroup('hour24');
+    final minute = match.namedGroup('minute24');
+
+    if (hour != null && minute != null) {
+      final hours = int.parse(hour);
+      final minutes = int.parse(minute);
+
+      return (hours, minutes);
+    }
+  }
+
+  return null;
 }
